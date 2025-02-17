750197ab4be23d08b5ddd8568464e205b15eccca
Joscha Hartmann <joscha.hartmann@web.de>
2025-02-17 21:41:45 +0100

    Add initial App, Header, Nav, Main and Footer component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/layout/Header";
+import Nav from "./components/layout/Nav";
+import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <nav className="nav">
+      <h1>Nav</h1>
+    </nav>
+  );
+}
+
+export default Nav;
