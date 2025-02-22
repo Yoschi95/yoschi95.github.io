b02ef743472cabfbc27cdced1672c2d67f03d711
Joscha Hartmann <joscha.hartmann@web.de>
2025-02-22 11:07:18 +0100

    Align navbar and add link to logo in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,14 @@
 import jh_logo from "../../assets/images/logo/jh_logo.svg";
 import Nav from "./Nav";
+import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
-      <img src={jh_logo} alt="Joscha Hartmann brand logo" />
+      <a href="#home">
+        <img src={jh_logo} alt="Joscha Hartmann brand logo" class="header-logo" />
+      </a>
+
       <Nav />
     </header>
   );
