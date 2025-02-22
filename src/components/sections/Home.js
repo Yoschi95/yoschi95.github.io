2b038c9ad6f07d69e085a455c39cdbb0836aae7c
Joscha Hartmann <joscha.hartmann@web.de>
2025-02-22 12:16:16 +0100

    Add image and style to home section

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,14 +1,19 @@
+import profile_img from "../../assets/images/me/me_real.jpg";
 import "./Home.scss";
 
 function Home() {
   return (
     <main className="home" id="home">
-      <h1>Hello! I'm Joscha.&#128075;</h1>
-      <h2>
-        a passionate software engineer form Germany. I like to develop innovative solutions that solve complex problems
-        and improve the user experience so that it puts a smile on our faces.
-      </h2>
-      <p>Read more</p>
+      <p className="home-text">
+        <h1>Hello! I'm Joscha.&#128075;</h1>
+        <h2>
+          a passionate software engineer form Germany. I like to develop innovative solutions that solve complex
+          problems and improve the user experience so that it puts a smile on our faces.
+        </h2>
+        <br></br>
+        <p>Read more button</p>
+      </p>
+      <img src={profile_img} alt="Profile image Joscha Hartmann" className="home-img" />
     </main>
   );
 }
