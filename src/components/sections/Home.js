f770d390e064b3d463d8a5b856b52afa61acee48
Joscha Hartmann <joscha.hartmann@web.de>
2025-02-22 12:47:24 +0100

    Use button in home section

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,5 +1,6 @@
 import profile_img from "../../assets/images/me/me_real.jpg";
 import "./Home.scss";
+import Button from "../shared/Button.js";
 
 function Home() {
   return (
@@ -7,11 +8,11 @@ function Home() {
       <p className="home-text">
         <h1>Hello! I'm Joscha.&#128075;</h1>
         <h2>
-          a passionate software engineer form Germany. I like to develop innovative solutions that solve complex
+          a passionate software engineer from Germany. I like to develop innovative solutions that solve complex
           problems and improve the user experience so that it puts a smile on our faces.
         </h2>
         <br></br>
-        <p>Read more button</p>
+        <Button text="Read more" />
       </p>
       <img src={profile_img} alt="Profile image Joscha Hartmann" className="home-img" />
     </main>
