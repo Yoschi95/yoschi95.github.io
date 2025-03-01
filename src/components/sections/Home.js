0e8bd5ac319348f32b1e595ceaa0f9af6bc986e7
Joscha Hartmann <joscha.hartmann@web.de>
2025-03-01 10:23:55 +0100

    Add ArrowDown component

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,6 +1,7 @@
 import profile_img from "../../assets/images/me/me_real.jpg";
 import "./Home.scss";
 import StandardButton from "../shared/StandardButton.js";
+import ArrowDown from "../shared/ArrowDown.js";
 
 function Home() {
   // Define function to scroll to the section when clicking the read more button
@@ -14,15 +15,20 @@ function Home() {
 
   return (
     <main className="home" id="home">
-      <p className="home-text">
-        <h1>Hello! I'm Joscha.&#128075;</h1>
-        <h2>
-          a passionate software engineer from Germany. I like to develop innovative solutions that solve complex
-          problems and improve the user experience so that it puts a smile on our faces.
-        </h2>
-        <StandardButton text="Read more" handleClick={scrollToAboutSection} />
-      </p>
-      <img src={profile_img} alt="Profile image Joscha Hartmann" className="home-img" />
+      <div className="home-flexbox">
+        <p className="home-text">
+          <h1>Hello! I'm Joscha.&#128075;</h1>
+          <h2>
+            a passionate software engineer from Germany. I like to develop innovative solutions that solve complex
+            problems and improve the user experience so that it puts a smile on our faces.
+          </h2>
+          <StandardButton text="Read more" handleClick={scrollToAboutSection} />
+        </p>
+        <img src={profile_img} alt="Profile image Joscha Hartmann" className="home-img" />
+      </div>
+      <div className="home-arrow">
+        <ArrowDown />
+      </div>
     </main>
   );
 }
---
+++ src/components/shared/ArrowDown.js
@@ -0,0 +1,11 @@
+import "./ArrowDown.scss";
+
+function ArrowDown() {
+  return (
+    <div className="circle">
+      <div className="arrow-down"></div>
+    </div>
+  );
+}
+
+export default ArrowDown;
