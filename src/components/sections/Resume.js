d216dbd5cc37858052ca941d3fc4b63409dd83d9
Joscha Hartmann <joscha.hartmann@web.de>
2025-03-07 18:36:41 +0100

    Use blob to adjust the file name when downloading the resume

--- src/components/sections/Resume.js
+++ src/components/sections/Resume.js
@@ -2,11 +2,32 @@ import "./Resume.scss";
 import CV_en_website from "pdf:/src/assets/docs/CV_en_website.pdf";
 
 function Resume() {
+  const handleDownload = async () => {
+    try {
+      // Fetch the PDF file from the frontend server
+      const response = await fetch(CV_en_website);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "CV_Joscha.pdf";
+
+      document.body.appendChild(a);
+      a.click();
+
+      a.remove();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading the PDF:", error);
+    }
+  };
+
   return (
     <div>
-      <a className="resumeButton" href={CV_en_website} download>
+      <button className="resumeButton" onClick={handleDownload}>
         Resume
-      </a>
+      </button>
     </div>
   );
 }
