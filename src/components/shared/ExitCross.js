6185ed2131075af8b86f902a459e1fc811955877
Joscha Hartmann <joscha.hartmann@web.de>
2025-03-21 19:54:23 +0100

    Add ExitCross component

---
+++ src/components/shared/ExitCross.js
@@ -0,0 +1,12 @@
+import "./ExitCross.scss";
+
+function ExitCross() {
+  return (
+    <div>
+      <span className="cross-line cross-line-1"></span>
+      <span className="cross-line cross-line-2"></span>
+    </div>
+  );
+}
+
+export default ExitCross;
