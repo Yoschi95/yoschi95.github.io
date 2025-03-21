f326877047e2e8525e12f36973d36931f899d0c8
Joscha Hartmann <joscha.hartmann@web.de>
2025-03-21 19:56:50 +0100

    Add projects to related section

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,9 +1,80 @@
 import "./Projects.scss";
+import ProjectCard from "../shared/ProjectCard";
 
 function Projects() {
   return (
     <main className="projects" id="projects">
       <h1>Projects</h1>
+      <div className="cards-container">
+        <ProjectCard
+          title={"Portfolio Website"}
+          description={
+            <ul>
+              <li>Development of series software in automotive sector</li>
+              <li>Test management</li>
+              <li>Software -architecture, -implementation and -testing</li>
+            </ul>
+          }
+          techStack={
+            <ul>
+              <li>C</li>
+              <li>Jenkins</li>
+              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
+            </ul>
+          }
+        />
+        <ProjectCard
+          title={"Litte Lemon"}
+          description={
+            <ul>
+              <li>Development of series software in automotive sector</li>
+              <li>Test management</li>
+              <li>Software -architecture, -implementation and -testing</li>
+            </ul>
+          }
+          techStack={
+            <ul>
+              <li>C</li>
+              <li>Jenkins</li>
+              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
+            </ul>
+          }
+        />
+        <ProjectCard
+          title={"PMM"}
+          description={
+            <ul>
+              <li>Development of series software in automotive sector</li>
+              <li>Test management</li>
+              <li>Software -architecture, -implementation and -testing</li>
+            </ul>
+          }
+          techStack={
+            <ul>
+              <li>C</li>
+              <li>Jenkins</li>
+              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
+            </ul>
+          }
+        />
+        <ProjectCard
+          title={"PisstsHeut"}
+          description={
+            <ul>
+              <li>Development of series software in automotive sector</li>
+              <li>Test management</li>
+              <li>Software -architecture, -implementation and -testing</li>
+            </ul>
+          }
+          techStack={
+            <ul>
+              <li>C</li>
+              <li>Jenkins</li>
+              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
+            </ul>
+          }
+        />
+      </div>
     </main>
   );
 }
