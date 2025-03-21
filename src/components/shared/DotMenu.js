2227e4016a7f2a8b91da572f830ea39532b4f3b9
Joscha Hartmann <joscha.hartmann@web.de>
2025-03-21 19:54:07 +0100

    Add DotMenu component

---
+++ src/components/shared/DotMenu.js
@@ -0,0 +1,13 @@
+import "./DotMenu.scss";
+
+function DotMenu() {
+  return (
+    <div className="dot-menu-container">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+  );
+}
+
+export default DotMenu;
