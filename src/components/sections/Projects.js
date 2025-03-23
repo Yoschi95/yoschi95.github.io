4cd4e9691aff44b83cef65b1b649785cdfed4570
Joscha Hartmann <joscha.hartmann@web.de>
2025-03-23 15:06:23 +0100

    Add tech stack to projects

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -11,39 +11,25 @@ function Projects() {
           description={
             "My portfolio website showcases my experience, skills and education in a clean and engaging design. It is fully repsonsive, fast and user-friendly."
           }
-          techStack={
-            <ul>
-              <li>C</li>
-              <li>Jenkins</li>
-              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
-            </ul>
-          }
+          techStack={"HTML, SCSS, JavaScript, React.js"}
+          gitHubLink={"https://github.com/Yoschi95/yoschi95.github.io"}
+          appLink={"https://yoschi95.github.io/"}
         />
         <ProjectCard
           title={"Little Lemon"}
           description={
             "A modern restaurant website that features a menu, opening hours, and an online reservation system. Customers can easily book tables online and reice confirmations"
           }
-          techStack={
-            <ul>
-              <li>C</li>
-              <li>Jenkins</li>
-              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
-            </ul>
-          }
+          techStack={"HTML, SCSS, JavaScript, React.js"}
+          gitHubLink={"https://github.com/Yoschi95/little-lemon"}
+          appLink={"https://yoschi95.github.io/little-lemon/"}
         />
         <ProjectCard
           title={"PMM"}
           description={
             "An ECU middleware designed to manage notifications within a vehicle's parking context. It allows for message prioritization, configurable display durations, and selection of the display where the message will appear"
           }
-          techStack={
-            <ul>
-              <li>C</li>
-              <li>Jenkins</li>
-              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
-            </ul>
-          }
+          techStack={"C, Enterprise Architect, Jenkins, CUnit, Atlassian Toolchain (Confluence, Bitbucket , Jira)"}
         />
         <ProjectCard
           title={"PisstsHeut"}
@@ -52,11 +38,11 @@ function Projects() {
           }
           techStack={
             <ul>
-              <li>C</li>
-              <li>Jenkins</li>
-              <li>Atlassian Toolchain (Confluence, Bitbucket , Jira)</li>
+              <li>Frontend: HTML, SCSS, JavaScript, React.js</li>
+              <li>Backend: TBD</li>
             </ul>
           }
+          gitHubLink={"https://github.com/Yoschi95/PisstsHeut"}
         />
       </div>
     </main>
