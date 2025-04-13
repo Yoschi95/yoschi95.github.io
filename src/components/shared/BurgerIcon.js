8e5ad6d2221f93fe4008ca46bd341816caa56913
Joscha Hartmann <joscha.hartmann@web.de>
2025-04-13 20:08:21 +0200

    Add BurgerIcon

---
+++ src/components/shared/BurgerIcon.js
@@ -0,0 +1,13 @@
+import "./BurgerIcon.scss";
+
+function BurgerIcon() {
+  return (
+    <div className="burger-icon">
+      <div className="burger-bar"></div>
+      <div className="burger-bar"></div>
+      <div className="burger-bar"></div>
+    </div>
+  );
+}
+
+export default BurgerIcon;
