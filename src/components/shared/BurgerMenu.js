88aa10dc581f4183f1384d36a984dd42f5ad4bd5
Joscha Hartmann <joscha.hartmann@web.de>
2025-04-14 17:37:01 +0200

    Add sidebar with sections to BurgerMenu

--- src/components/shared/BurgerMenu.js
+++ src/components/shared/BurgerMenu.js
@@ -1,12 +1,28 @@
 import "./BurgerMenu.scss";
+import { useState } from "react";
 import BurgerIcon from "./BurgerIcon";
+import ExitCross from "../shared/ExitCross";
+import Nav from "../sections/Nav";
 
 function BurgerMenu({ className }) {
+  const [isSideBarOpen, setSideBarOpen] = useState(false);
+
   return (
     <div className={`${className} burger-menu`}>
-      <button className="burger-button">
-        <BurgerIcon />
-      </button>
+      {isSideBarOpen ? (
+        <div className="burger-sidebar">
+          <button className="exit-cross-button" onClick={() => setSideBarOpen(false)}>
+            <ExitCross />
+          </button>
+          <div className="burger-navbar">
+            <Nav />
+          </div>
+        </div>
+      ) : (
+        <button className="burger-button" onClick={() => setSideBarOpen(true)}>
+          <BurgerIcon />
+        </button>
+      )}
     </div>
   );
 }
